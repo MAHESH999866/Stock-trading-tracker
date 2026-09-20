3fa5343a2134cee3faf29bcfcee341e43cf262d1
Durga MaheswaraRao Pulicharla <dmsraop@gmail.com>
2026-09-20 23:22:51 +0530

    Time: 1 ms (100%), Space: 44.1 MB (34.29%) - LeetHub

---
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int reverseDegree(String s) { int sum = 0;
+        for(int i=0;i<s.length();i++){
+            char ch = s.charAt(i);
+            int value = 26- (ch - 'a');
+            int position = i+1;
+            sum += value * position;
+        }
+        return sum;
+    }
+}
